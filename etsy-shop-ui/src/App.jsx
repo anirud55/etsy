e651bd9db8f56b28210702bd1d56d56e82d3347a
anirud55 <90978843+anirud55@users.noreply.github.com>
2022-03-11 01:10:24 -0800

    front-end

--- etsy-shop-ui/src/App.jsx
+++ etsy-shop-ui/src/App.jsx
@@ -1,7 +1,29 @@
 import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const App = () => {
-  return <Home/>;
+  const user = useSelector((state) => state.user.currentUser);
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/login">{user ? <Navigate to="/" /> : <Login />}</Route>
+        <Route path="/register">
+          {user ? <Navigate to="/" /> : <Register />}
+        </Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
