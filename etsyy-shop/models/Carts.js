732b24159a15c4aa8bc1802de7ca46b75a3c1fc4
anirud55 <90978843+anirud55@users.noreply.github.com>
2022-03-14 01:36:22 -0700

    updated models

---
+++ etsyy-shop/models/Carts.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+    const Cart = sequelize.define("Cart",{
+        cartid: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        userid: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        productid: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+    });
+    return Cart;
+}
---
+++ etsyy-shop/models/Product.js
@@ -0,0 +1,48 @@
+module.exports = (sequelize, DataTypes) => {
+    const Product = sequelize.define("Product",{
+        productid: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        categoryid: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            required: true
+        },
+        desc: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        img: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        size: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        color: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            required: true
+        },
+        inStock: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            default: 0,
+        },
+
+    });
+    return Product;
+};
