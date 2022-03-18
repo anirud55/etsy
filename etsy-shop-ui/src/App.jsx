4b6a6d594aeb6cb14339665064e4c7300f7145ff
anirud55 <90978843+anirud55@users.noreply.github.com>
2022-03-18 12:03:41 -0700

    Update App.jsx

--- etsy-shop-ui/src/App.jsx
+++ etsy-shop-ui/src/App.jsx
@@ -1,5 +1,7 @@
 import Home from "./pages/Home";
 import Register from "./pages/Register";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
 import Login from "./pages/Login";
 import {
   BrowserRouter as Router,
@@ -16,6 +18,8 @@ const App = () => {
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path="/product/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
         <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
         <Route path="/register" element={user ? <Navigate to="/" /> : <Register />} />
       </Routes>
